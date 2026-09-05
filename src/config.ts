1e1458c2b2179b1c3799b7a4f7c0ac62ec6be3a6
Yuanzhi SHAO <shaoyuanzhi@anosi.cn>
2026-09-05 22:53:19 +0800

    chore:更换banner

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,7 @@ export const siteConfig: SiteConfig = {
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: true, // Display the credit text of the banner image
-			text: "谁人不爱沪上白切鸡", // Credit text to be displayed
+			text: "不忘初心", // Credit text to be displayed
 			url: "", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
